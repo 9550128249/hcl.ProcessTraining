11673e04d9a5a8e5f1d9f19fa85e745d3ddcaa94
9550128249 <69156317+9550128249@users.noreply.github.com>
2020-12-28 14:23:01 +0530

    Add files via upload

---
+++ College1.java
@@ -0,0 +1,19 @@
+package org.hcl.maven3;
+import java.util.List;
+public class College1 {
+	private List<String>coursesoffered;
+public College1() {
+	// TODO Auto-generated constructor stub
+}
+public College1(List<String> coursesoffered) {
+	super();
+	this.coursesoffered = coursesoffered;
+}
+public List<String> getCoursesoffered() {
+	return coursesoffered;
+}
+public void setCoursesoffered(List<String> coursesoffered) {
+	this.coursesoffered = coursesoffered;
+}
+
+}
